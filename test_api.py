1b073f25f4869b4e24ca7c4b6432efd118c4127c
Yijia Wang <yijiawang@yijias-mac-mini.mynetworksettings.com>
2023-12-19 11:04:40 -0500

    add a pytest module

---
+++ test_api.py
@@ -0,0 +1,23 @@
+import pytest
+
+from app import app as flask_app
+
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_users(app, client):
+    res = client.get('/users')
+    assert res.status_code == 200
+
+
+def test_admin_user(app, client):
+    res = client.get('/jobs/yw3936@columbia.edu')
+    assert res.status_code == 200
